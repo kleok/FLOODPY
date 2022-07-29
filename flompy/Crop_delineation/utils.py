91349c3f9a61f8c906a8f81fc5d4e4439d5f21fc
alekfal <alek.falagas@gmail.com>
2022-07-29 19:27:07 +0300

    Continue intergration (#5)

--- flompy/Crop_delineation/utils.py
+++ flompy/Crop_delineation/utils.py
@@ -9,6 +9,8 @@ import requests
 from requests import exceptions
 import json
 import shapely.wkt
+import rasterio
+from rasterio.warp import reproject, Resampling
 
 def equation_3(fpaths:list, kernel_N_weight:pd.DataFrame)->list:
     """Square root of gradient between 2 pixels, in selected orientantion, for entire image.
@@ -281,3 +283,37 @@ def corine(aoi:str, to_file:bool = False, fname:str = "corine_2018.shp")->tuple:
         data.to_file(fname)
     
     return data, fname
+
+def reproj_match(image:str, base:str, outfile:str, resampling:rasterio.warp.Resampling = Resampling.nearest) -> None:
+    """Reprojects/Resamples an image to a base image.
+    Args:
+        image (str): Path to input file to reproject/resample
+        base (str): Path to raster with desired shape and projection 
+        outfile (str): Path to saving Geotiff
+    """
+    # open input
+    with rasterio.open(image) as src:
+        # open input to match
+        with rasterio.open(base) as match:
+            dst_crs = match.crs
+            dst_transform = match.meta["transform"]
+            dst_width = match.width
+            dst_height = match.height
+        # set properties for output
+        metadata = src.meta.copy()
+        metadata.update({"crs": dst_crs,
+                           "transform": dst_transform,
+                           "width": dst_width,
+                           "height": dst_height,
+                           })
+        with rasterio.open(outfile, "w", **metadata) as dst:
+            # iterate through bands and write using reproject function
+            for i in range(1, src.count + 1):
+                reproject(
+                    source = rasterio.band(src, i),
+                    destination = rasterio.band(dst, i),
+                    src_transform = src.transform,
+                    src_crs = src.crs,
+                    dst_transform = dst_transform,
+                    dst_crs = dst_crs,
+                    resampling = resampling)
