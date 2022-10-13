7de371928bc1ddd094ea0157ba12767d9fd3df6a
Alekos Falagas <alek.falagas@gmail.com>
2022-10-13 19:46:40 +0300

    Remove useless docstrings (#15)

--- flompy/Preprocessing_S2_data/exceptions.py
+++ flompy/Preprocessing_S2_data/exceptions.py
@@ -1,26 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
-Copyright (C) 2021-2022 by K.Karamvasis
-Email: karamvasis_k@hotmail.com
 
-Authors: Karamvasis Kleanthis, Alekos Falagas
-
-This file is part of FLOMPY - FLOod Mapping PYthon toolbox.
-
-    FLOMPY is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    FLOMPY is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with FLOMPY. If not, see <https://www.gnu.org/licenses/>.
-"""
 class timeseriesError(Exception):
     """ Base class for exceptions in this module."""
     pass
