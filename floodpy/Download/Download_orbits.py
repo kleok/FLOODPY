a9f182bc4bc9e0cac36182457c088399444d362f
kleok <karamvasis_k@hotmail.com>
2022-12-19 18:52:38 +0200

    Changing Sentinel-1 data folder naming

--- floodpy/Download/Download_orbits.py
+++ floodpy/Download/Download_orbits.py
@@ -94,17 +94,17 @@ def downloads_orbit_filename(orbit_dictionary, snap_orbit_dir,S1_filename, S1_da
             
     
  
-def download_orbits(snap_dir:str, temp_export_dir:str, S1_GRD_dir:str)->None:
+def download_orbits(snap_dir:str, temp_export_dir:str, S1_dir:str)->None:
     """
     Download Sentinel-1 orbits
 
     Args:
         snap_dir (str): Directory that snap stores Sentinel-1 orbits files.
         temp_export_dir (str): Temporary directory.
-        S1_GRD_dir (str): Directory the Sentinel-1 images are stored.
+        S1_dir (str): Directory the Sentinel-1 images are stored.
     """
     
-    S1_products = os.path.join(S1_GRD_dir,'S1_products.csv')
+    S1_products = os.path.join(S1_dir,'S1_products.csv')
     assert os.path.exists(S1_products)
     S1_filenames=pd.read_csv(S1_products)
 
