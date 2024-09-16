afc1c3af74a05071e43cd2c7e19eec4405791409
kleok <karamvasis_k@hotmail.com>
2024-09-16 16:55:17 +0300

    generate vector file for flooded regions

--- floodpy/utils/geo_utils.py
+++ floodpy/utils/geo_utils.py
@@ -1,4 +1,28 @@
 from shapely.geometry import Polygon
+import geopandas as gpd
+from rasterio.features import shapes
+from shapely.geometry import shape
+import rasterio
+import numpy as np
 
 def create_polygon(coordinates):
     return Polygon(coordinates['coordinates'][0])
+
+
+def convert_to_vector(Floodpy_app):
+    with rasterio.open(Floodpy_app.Flood_map_dataset_filename) as src:
+        data = src.read(1).astype(np.int16)
+
+        # Use a generator instead of a list
+        shape_gen = ((shape(s), v) for s, v in shapes(data, transform=src.transform))
+
+        # either build a pd.DataFrame
+        # df = DataFrame(shape_gen, columns=['geometry', 'class'])
+        # gdf = GeoDataFrame(df["class"], geometry=df.geometry, crs=src.crs)
+
+        # or build a dict from unpacked shapes
+        gdf = gpd.GeoDataFrame(dict(zip(["geometry", "flooded_regions"], zip(*shape_gen))), crs=src.crs)
+        gdf = gdf.loc[gdf.flooded_regions == 1,:]
+        gdf.datetime = Floodpy_app.flood_datetime_str
+
+        gdf.to_file(Floodpy_app.Flood_map_vector_dataset_filename, driver='GeoJSON')
